80e783b3a52459eaebe8d80b8ba4bf7e186a0b40
duclong09 <duclongsew@gmail.com>
2023-02-08 18:17:41 +0700

    Go basic

---
+++ array/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	// points := [...]int{7,9,10,11,12}
+	// fmt.Printf("%v, %T\n", points,points)
+
+
+
+	listUser := [] int{2,4,6,8,10,12,14}
+	//listProduct := listUser
+
+	//ngan xep va hang doi:
+	//Xoa cuoi: :6
+	//listProduct := listUser[:5]
+	listProduct := listUser[1:]
+	//fmt.Println(listUser)
+	fmt.Println(listProduct)
+}
